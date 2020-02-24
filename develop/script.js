65dbf2f3917848a555863f15153b0e5ec332f336
Annaruth McBride <armcbride17@gmail.com>
2020-02-24 13:05:42 -0600

    variables for password generation established

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Beginning of password building, prompts
+var stringLength = prompt("Enter a number between 7 and 120 for your password length.")
+
+//tests for stringLength variable - works
+console.log(stringLength);
+
+//prompts for the password criteria
+if (stringLength > 6 && stringLength < 121) {
+  alert("Your password will be " + stringLength + " characters long. For the following prompts, select which elements you would like your password to include.")
+
+  var lowerCase = confirm("Lower case letters?");
+  var upperCase = confirm("Upper case letters?");
+  var specialCase = confirm("Special Characters?");
+  var numberCase = confirm("Numbers?");
+}
+else { 
+  stringLength = prompt("Enter a number between 7 and 120 for your password length.")
+}
+//tests input for password prompts - works
+console.log(lowerCase, upperCase, specialCase, numberCase);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +33,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Password input types for randomizer
+var charTypes = {
+  'upper': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  'lower': 'abcdefghijklmnopqrstuvwxyz',
+  'number': '0123456789',
+  'special': '!$%^&*()-=+[]{};#:@~,./<>?'
+};
+
